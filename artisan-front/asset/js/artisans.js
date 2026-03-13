db3f2331e6500d443ca161a659adc3be53007b48
Lydie-pixel <fleursdesiles7@gmail.com>
2026-03-13 17:22:35 +0100

    Première mise en place des routes en fonction des choix dans l'app

--- artisan-front/asset/js/artisans.js
+++ artisan-front/asset/js/artisans.js
@@ -1,42 +1,81 @@
+const params = new URLSearchParams(window.location.search);
+const categorieId = params.get("categorie");
+
+function createArtisanCard(artisan) {
+
+    const card = document.createElement("div");
+    card.classList.add("col-md-4");
+
+    card.innerHTML = `
+    <div class="card h-100 shadow">
+        <div class="card-body">
+
+            <div class="mb-2 text-warning">
+                ⭐ ${artisan.note}
+            </div>
+
+            <h5>${artisan.nom}</h5>
+
+            <span>
+                Spécialité : ${artisan.specialite.nom}
+                <br>
+                Ville : ${artisan.ville}
+            </span>
+
+            <br><br>
+
+            <a class="btn btn-primary" href="artisanDetail.html?id=${artisan.id}">
+                Voir sa fiche
+            </a>
+
+        </div>
+    </div>
+    `;
+
+    return card;
+}
+
+
 console.log("artisans.js chargé");
-fetch("http://localhost:3000/api/artisans")
-  .then(response => response.json())
-  .then(artisans => {
+
+let url = "http://localhost:3000/api/artisans";
+
+if (categorieId) {
+    url = `http://localhost:3000/api/categorie/${categorieId}`;
+}
+
+fetch(url)
+.then(res => res.json())
+.then(data => {
 
     const container = document.getElementById("artisans-list");
 
-    artisans.forEach(artisan => {
-
-      const card = document.createElement("div");
-
-      card.innerHTML = `
-      <div class="card h-100 shadow">
-        <div class="card-body">
-
-          <div class="mb-2 text-warning">
-            ⭐ ${artisan.note}
-          </div>
-
-          <h5>${artisan.nom}</h5>
-
-          <span>
-            Spécialité : ${artisan.specialite.nom}
-            <br>
-            Ville : ${artisan.ville}
-          </span>
-
-          <br><br>
-
-          <a class="btn btn-primary" href="artisanDetail.html?id=${artisan.id}">
-            Voir sa fiche
-          </a>
-
-        </div>
-      </div>
-      `;
-
-      container.appendChild(card);
-
+    data.forEach(artisan => {
+        container.appendChild(createArtisanCard(artisan));
     });
 
-  });
+});
+
+fetch("http://localhost:3000/api/artisans/mois")
+.then(res => res.json())
+.then(data => {
+
+    const container = document.getElementById("top-artisans");
+
+    data.forEach(artisan => {
+        container.appendChild(createArtisanCard(artisan));
+    });
+
+});
+
+fetch(`http://localhost:3000/api/artisans/categorie/${id}`)
+.then(res => res.json())
+.then(data => {
+
+    const container = document.getElementById("categorie");
+
+    data.forEach(artisan => {
+        container.appendChild(createArtisanCard(artisan));
+    });
+
+});
